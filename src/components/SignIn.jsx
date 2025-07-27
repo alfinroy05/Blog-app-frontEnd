bb59569b7aa1901aeddd813bbce97177dfdfe272
alfinroy05 <alfinroy2005@gmail.com>
2025-07-28 00:17:18 +0530

    sign in page hafly completed

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div><div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                         <label htmlFor="" className="form-label">Password</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">SignIn</button>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <a href="/signup" className="btn btn-secondary">new User click here :)</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div></div>
+  )
+}
+
+export default SignIn
